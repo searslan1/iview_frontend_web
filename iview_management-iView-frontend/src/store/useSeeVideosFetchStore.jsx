7ae56de9a6c89d8031f91f18f2869951a5c55592
searslan1 <169062883+searslan1@users.noreply.github.com>
2024-11-19 13:35:39 +0300

    new

--- iview_management-iView-frontend/src/store/useSeeVideosFetchStore.jsx
+++ iview_management-iView-frontend/src/store/useSeeVideosFetchStore.jsx
@@ -2,16 +2,19 @@
 import axios from 'axios';
 import create from 'zustand';
 
+// .env dosyasındaki VITE_API_URL'i kullan
+const API_URL = import.meta.env.VITE_API_URL;
+
 const useSeeVideosStore = create((set) => ({
   videoData: [],
 
   fetchVideo: async (formId) => {
     try {
-      const response = await axios.get(`http://localhost:5000/api/videos/${formId}`);
+      const response = await axios.get(`${API_URL}/api/videos/${formId}`);
       const videos = await Promise.all(
         response.data.map(async (video) => {
           // Request presigned URL for each video
-          const presignedUrlResponse = await axios.get(`http://localhost:5000/api/presigned-url/${video.videoKey}`);
+          const presignedUrlResponse = await axios.get(`${API_URL}/api/presigned-url/${video.videoKey}`);
           return {
             candidateId: video.candidateId,
             uploadDate: video.uploadDate,
