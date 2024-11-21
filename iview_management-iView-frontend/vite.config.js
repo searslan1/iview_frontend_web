513d353c6e3b92e28ccbc42fecf6b8b9569cd9c7
searslan1 <169062883+searslan1@users.noreply.github.com>
2024-11-21 17:01:56 +0300

    Endupdate

--- iview_management-iView-frontend/vite.config.js
+++ iview_management-iView-frontend/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const API_URL = import.meta.env.VITE_API_URL;
+
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: API_URL,
-        changeOrigin: true,
-      },
-    },
+  build: {
+    outDir: 'dist', 
   },
-});
+})
